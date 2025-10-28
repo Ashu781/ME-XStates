9cb5db3445cc4650221d5612ed887acc6517c9c1
Ashutosh Agrawal <ashu@AshutoshMac.local>
2025-10-28 22:22:48 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,101 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
+
 
 function App() {
+
+  let [country, setCountry] = useState("");
+  let [state, setState] = useState("");
+  let [city, setCity] = useState("");
+  let [display, setDisplay] = useState(false);
+
+  let [countryList,setCountryList] = useState([]);
+  let [stateList,setStateList] = useState([]);
+  let [cityList,setCityList] = useState([]);
+  
+
+    async function getState(country){
+      let responseState = await axios.get(`https://crio-location-selector.onrender.com/country=${country}/states`);
+      setStateList(responseState.data);
+    }
+
+    async function getCity(country, state){
+      let responseCity = await axios.get(`https://crio-location-selector.onrender.com/country=${country}/state=${state}/cities`);
+      setCityList(responseCity.data);
+    }
+
+
+  let handleCountryChange = (e)=>{
+         setState("");
+         setCity("");
+         setCountry(e.target.value);
+         getState(e.target.value);
+  }
+
+  let handleStateChange = (e)=>{
+         setCity("");
+         setState(e.target.value);
+         getCity(country, e.target.value);
+  }
+
+  let handleCityChange = (e)=>{
+         setCity(e.target.value);
+         setDisplay(true);
+  }
+
+  useEffect(()=>{
+    
+    async function getCountry(){
+      let responseCountry = await axios.get("https://crio-location-selector.onrender.com/countries");
+      setCountryList(responseCountry.data);
+    }
+
+    getCountry();
+
+  },[]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <>
+      <select value={country} onChange={handleCountryChange}>
+        <option value="" key="">Select Country</option>
+        {
+          countryList.map((ele)=>(
+             <option value={ele} key={ele}>{ele}</option>
+          ))
+        }
+      </select>
+
+
+
+
+      <select value={state} onChange={handleStateChange} disabled={!country}>
+        <option value="" key="">Select State</option>
+        {
+          stateList.map((ele)=>(
+             <option value={ele} key={ele}>{ele}</option>
+          ))
+        }
+      </select>
+
+
+
+
+      <select value={city} onChange={handleCityChange} disabled={!state}>
+        <option value="" key="">Select City</option>
+        {
+          cityList.map((ele)=>(
+             <option value={ele} key={ele}>{ele}</option>
+          ))
+        }
+      </select>
+
+
+
+      {
+        display && <h3>You selected {city}, {state}, {country}</h3>
+      }
+
+   </>
   );
 }
 
